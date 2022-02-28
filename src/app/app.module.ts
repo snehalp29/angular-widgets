7d02e7a50073cdca23168a1d79ec4f8cea8ab60c
snehalp29 <snehalpatkar@gmail.com>
2022-02-27 21:57:13 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { HelloComponent } from './hello.component';
 import { WidgetsComponent } from '../widgets/widgets.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterailModule } from '../material.module';
+import { WidgetsListComponent } from 'src/widgets/widgets-list/widgets-list.component';
+import { WidgetDetailsComponent } from 'src/widgets/widget-details/widget-details.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MaterailModule, AppRoutingModule],
-  declarations: [AppComponent, HelloComponent, WidgetsComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
+    WidgetDetailsComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
